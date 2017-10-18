e6671610692b226231edfbc4546c7f7c349ed8aa
Agnivesh <agnivesh@betatestsolutions.com>
2017-10-18 12:20:57 +0530

    g devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   get 'home' => 'home#index'
   namespace :api, defaults: { format: :json } do
     resources :quotes, only: [ :show ]
